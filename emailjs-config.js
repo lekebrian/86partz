61e4509f827802675b4d74eba011d5300ad9b540
bbrian23 <lekebryan64@gmail.com>
2026-02-16 20:41:41 +0100

    Ok

--- emailjs-config.js
+++ emailjs-config.js
@@ -2,10 +2,10 @@
 // 1. Get your Public Key from EmailJS: https://dashboard.emailjs.com/admin/account
 // 2. Create a template and set its ID below (see EMAILJS_ORDER_TEMPLATE.md for HTML and variables)
 window.EMAILJS_ORDER_CONFIG = {
-  serviceId: 'service_xtrt6uf',
-  templateId: 'template_7zm535n',
+  serviceId: 'service_3mjwy48',
+  templateId: 'template_uzhb0ao',
   publicKey: 'cGQL-VJm_U10wFXH9',
-  toEmail: 'orders@86partz.com',    // Recipient (also sent as template var for template override)
-  fromEmail: 'orders@86partz.com', // Sender address (if your template/service allows overriding)
+  toEmail: '86partz.official@gmail.com',    // Recipient (also sent as template var for template override)
+  fromEmail: '86partz.official@gmail.com', // Sender address (if your template/service allows overriding)
   fromName: '86Partz Orders',      // Optional: "From" display name (if your template uses it)
 };
